e6e897c16cc5fedaf7adb214222682ad29e3ba22
Accretian <kvik.ocean@gmail.com>
2025-04-25 14:54:07 +0300

    1

--- Main.cpp
+++ Main.cpp
@@ -3,18 +3,18 @@
 
 std::pair<size_t, size_t> MatrixArgMax(const std::vector<std::vector<int>>& matrix) {
 	int max = 0;
-	int i1 = 0;
-	int j1 = 0;
+	int imax = 0;
+	int jmax = 0;
 	for (int i = 0; i != (int)matrix.size(); i++) {
 		for (int j = 0; j != (int)matrix[i].size(); j++) {
 			if (matrix[i][j] > max) {
 				max = matrix[i][j];
-					i1 = i;
-					j1 = j;
+					imax = i;
+					jmax = j;
 			}
 		}
 	}
-	return {i1, j1};
+	return {imax, jmax};
 }
 
 int main() {

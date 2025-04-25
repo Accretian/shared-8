88bf86a3ddecf879641802b1e8c36c5d1d7afc14
Accretian <kvik.ocean@gmail.com>
2025-04-25 14:50:16 +0300

    1

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
+#include <vector>
 
-enum class EnumTest {
-
-};
-
-
-static class TestClass {
-
-};
-
-static void testmethod() {
-
+std::pair<size_t, size_t> MatrixArgMax(const std::vector<std::vector<int>>& matrix) {
+	int max;
+	int i1 = 0;
+	int j1 = 0;
+	for (int i = 0; i != (int)matrix.size(); i++) {
+		for (int j = 0; j != (int)matrix[i].size(); j++) {
+			if (matrix[i][j] > max) {
+				max = matrix[i][j];
+					i1 = i;
+					j1 = j;
+			}
+		}
+	}
+	return {i1, j1};
 }
 
-
 int main() {
+	int a, b;
+	std::cin >> a >> b;
+	std::vector<std::vector<int>> vector(a, std::vector<int>(b));
+
+	for (int i = 0; i != a; i++) {
+		for (int j = 0; j != b; j++) {
+			std::cin >> vector[i][j];
+		}
+	}
+
+	std::pair<size_t, size_t> pair = MatrixArgMax(vector);
+	std::cout << pair.first << " " << pair.second;
+
+
 
 
 
